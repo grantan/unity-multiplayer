bac7902e03292572f116df98311dd9d7bcfa6f21
Andrew Grant <grantandyt@gmail.com>
2018-01-11 22:03:07 -0700

    intial commit of unity multiplayer from scratch

---
+++ MultiplayerScratch/Assets/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        if(!isLocalPlayer) 
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal")*0.1f;
+        var z = Input.GetAxis("Vertical")*0.1f;
+
+        transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // This [Command] code is run on the server!
+
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+
+        // spawn the bullet on the clients
+       NetworkServer.Spawn(bullet);
+        
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 2.0f);        
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
